89ec0ad061dc6521626062bf09c1346eb5751e67
Eric Terrell <eric@terrell.info>
2025-08-25 00:58:23 -0600

    Added tests to ensure compatibility of Vault 3 documents with other (non Java) platforms.

--- Java Vault/Vault/src/jUnitTests/CryptoTests.java
+++ Java Vault/Vault/src/jUnitTests/CryptoTests.java
@@ -24,6 +24,7 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -43,9 +44,32 @@ import org.junit.Test;
  * Tests for the CryptoUtils class. 
  */
 public class CryptoTests {
-	final static String PASSWORD = "PASSWORD";
+	private final static String PASSWORD = "PASSWORD1234";
 
-	@Test
+    private final static byte[] NONRANDOM_SALT = new byte[] {
+            (byte) 0x45, (byte) 0x22, (byte) 0x00, (byte) 0xff, (byte) 0x11, (byte) 0xfa, (byte) 0x81, (byte) 0x88,
+            (byte) 0x04, (byte) 0x89, (byte) 0x99, (byte) 0x11, (byte) 0x07, (byte) 0x14, (byte) 0xcc, (byte) 0xdd
+    };
+
+    private final static byte[] NONRANDOM_IV = new byte[] {
+            (byte) 0x78, (byte) 0x90, (byte) 0x43, (byte) 0x42, (byte) 0x11, (byte) 0x09, (byte) 0x87, (byte) 0x82,
+            (byte) 0x73, (byte) 0x19, (byte) 0x34, (byte) 0x17, (byte) 0x32, (byte) 0x32, (byte) 0xdd, (byte) 0xff
+    };
+
+    private final static byte[] PLAINTEXT = new byte[] {
+            (byte) 0xaa, (byte) 0xbb, (byte) 0xdd, (byte) 0xcc, (byte) 0x11, (byte) 0x00, (byte) 0x30, (byte) 0x03,
+            (byte) 0x9a, (byte) 0xa0, (byte) 0xdd, (byte) 0x05, (byte) 0x05, (byte) 0x55, (byte) 0x77, (byte) 0x72,
+            (byte) 0x79, (byte) 0x29, (byte) 0x33, (byte) 0x01, (byte) 0x88, (byte) 0x99, (byte) 0xf9, (byte) 0x99
+    };
+
+    private final static byte[] CIPHERTEXT = new byte[] {
+            (byte)  45, (byte) 191, (byte) 169, (byte) 114, (byte) 204, (byte) 128, (byte) 152, (byte) 135, (byte)  53,
+            (byte)  32, (byte) 254, (byte) 163, (byte) 247, (byte)  44, (byte)  19, (byte)  71, (byte)  88, (byte) 119,
+            (byte)  33, (byte) 135, (byte) 180, (byte)  63, (byte)  25, (byte) 151, (byte) 108, (byte) 143, (byte) 192,
+            (byte)  37, (byte) 145, (byte) 164, (byte) 147, (byte)  5
+    };
+
+    @Test
 	public void roundTrip() throws NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidKeySpecException {
 		final String plainText = "Robert Eric Terrell is my name.";
 
@@ -100,4 +124,70 @@ public class CryptoTests {
 		final String decryptedText = CryptoUtils.decryptString(decryptionCipher, cipherText);
 		Assert.assertEquals(StringLiterals.EmptyString, decryptedText);
 	}
+
+    @Test
+    public void testInteropWithOtherPlatformsEncrypt() throws InvalidAlgorithmParameterException, NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, IllegalBlockSizeException,
+            BadPaddingException {
+        final Cipher encryptionCipher = CryptoUtils.createEncryptionCipher(PASSWORD,
+                VaultDocumentVersion.getLatestVaultDocumentVersion(), NONRANDOM_SALT, NONRANDOM_IV);
+
+        final byte[] cipherText = CryptoUtils.encrypt(encryptionCipher, PLAINTEXT);
+
+        Assert.assertArrayEquals(CIPHERTEXT, cipherText);
+    }
+
+    @Test
+    public void testInteropWithOtherPlatformsDecrypt() throws IllegalBlockSizeException, BadPaddingException,
+            InvalidAlgorithmParameterException, NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidKeySpecException, InvalidKeyException {
+        final Cipher decryptionCipher = CryptoUtils.createDecryptionCipher(PASSWORD,
+                VaultDocumentVersion.getLatestVaultDocumentVersion(),
+                NONRANDOM_SALT, NONRANDOM_IV);
+
+        final byte[] plainText = CryptoUtils.decrypt(decryptionCipher, CIPHERTEXT);
+
+        Assert.assertArrayEquals(PLAINTEXT, plainText);
+    }
+
+    @Test
+    public void TestEncryptDecryptStringRoundTrip() throws InvalidAlgorithmParameterException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException {
+        final Cipher encryptionCipher = CryptoUtils.createEncryptionCipher(PASSWORD,
+                VaultDocumentVersion.getLatestVaultDocumentVersion(), NONRANDOM_SALT, NONRANDOM_IV);
+
+        final Cipher decryptionCipher = CryptoUtils.createDecryptionCipher(PASSWORD,
+                VaultDocumentVersion.getLatestVaultDocumentVersion(),
+                NONRANDOM_SALT, NONRANDOM_IV);
+
+        String[][] plainTexts = new String[][]
+        {
+                {
+                    "Eric R. Terrell is my name",
+                    "Ni91Io9MCfPLxKqAC+Lp4RrQ6uu8/+VJ3WA62wm2QlU="
+                },
+                {
+                    "床前明月光，\n\n疑是地上霜。\n\n举头望明月，\n\n低头思故乡",
+                    "94TwqoYgs6M8vjJptNsoXlaC7uQbkIkWPc4Krfr2R0LcMNCC2Zs301eDo4HXqk0xD2FH/QxfCqyXdFqSkcE9jNvZXMr9iVweGmUItBu5zh8="
+                },
+                {
+                    "وأعرف أن الطريق إلى المستحيل طويـل",
+                    "KFv6Typ8vP4kARwSBAZiQpcbSbvLUYQkuEHkgUIjkmgFPt/A3d2jy7t57kfXjltSLKM9dTdmwxzfLbF3KfL84g=="
+                }
+        };
+
+        Arrays.stream(plainTexts).forEach(plainText -> {
+            try {
+                String cipherText = CryptoUtils.encryptString(encryptionCipher, plainText[0]);
+
+                // Verify the ciphertext to ensure that the encoding will work on other platforms.
+                Assert.assertEquals(plainText[1], cipherText);
+
+                String decryptedText = CryptoUtils.decryptString(decryptionCipher, cipherText);
+
+                Assert.assertEquals(plainText[0], decryptedText);
+            } catch (Exception ex) {
+                Assert.fail();
+            }
+        });
+    }
 }
